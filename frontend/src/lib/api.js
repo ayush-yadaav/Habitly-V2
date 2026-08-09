1966510a797d4a83883beb8465c12461c99b07fc
Ayush Yadav <yadavayush2091a@gmail.com>
2026-08-10 01:48:43 +0530

    add backend api

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,10 +1,43 @@
+// import axios from "axios";
+
+// const api = axios.create({ baseURL: "/api" });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("habitly_token");
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// api.interceptors.response.use(
+//   (res) => res,
+//   (err) => {
+//     if (err.response?.status === 401) {
+//       localStorage.removeItem("habitly_token");
+//       localStorage.removeItem("habitly_user");
+//       if (!window.location.pathname.startsWith("/welcome")) {
+//         window.location.href = "/welcome";
+//       }
+//     }
+//     return Promise.reject(err);
+//   }
+// );
+
+// export default api;
+
+
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const api = axios.create({
+  baseURL: "https://habitly-v2.onrender.com/api",
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("habitly_token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
@@ -14,10 +47,12 @@ api.interceptors.response.use(
     if (err.response?.status === 401) {
       localStorage.removeItem("habitly_token");
       localStorage.removeItem("habitly_user");
+
       if (!window.location.pathname.startsWith("/welcome")) {
         window.location.href = "/welcome";
       }
     }
+
     return Promise.reject(err);
   }
 );
